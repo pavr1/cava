2372c5c284695dba13cc974fab187a3887f706d4
Pablo Villalobos <pablo.villalobos@e2eanalytics.com>
2021-01-27 13:06:16 -0600

    Pages creation and design patterns

---
+++ cava/Controllers/BarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cava.Controllers
+{
+    public class BarController : Controller
+    {
+        // GET: Bar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
