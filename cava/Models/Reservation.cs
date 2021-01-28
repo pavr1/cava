3a07e8889416d2e225279310dbe9ec66fce4d96d
Pablo Villalobos <pablo.villalobos@e2eanalytics.com>
2021-01-27 20:06:26 -0600

    Bootstrap and jquery updates, besides Reservation Model creation

--- cava/Models/Reservation.cs
+++ cava/Models/Reservation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,27 @@ namespace cava.Models
 {
     public class Reservation
     {
+        public int ReservationId { get; set; }
+        [Required]
+        [Display(Name = "Fecha de Reservación")]
+        public DateTime Date { get; set; }
+        [Required]
+        [Display(Name = "Número de Personas")]
+        public int NumberOfPeople { get; set; }
+        [Required]
+        [Display(Name = "Nombre")]
+        public string ReserverFirstName { get; set; }
+        [Required]
+        [Display(Name = "Primer Apellido")]
+        public string ReserverMiddleName { get; set; }
+        [Required]
+        [Display(Name = "Segundo Apellido")]
+        public string ReserverLastName { get; set; }
+        [Required]
+        [Display(Name = "Número Telefónico")]
+        public string Phone { get; set; }
+        [Required]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
     }
 }
